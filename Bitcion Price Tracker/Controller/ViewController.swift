0877bb00aa2b263381bd6796852c2f8a7c77698f
Vikas Shetty <vikasrs4u@gmail.com>
2018-08-09 15:39:55 -0700

    Made changes to call the service and parse the data.

--- Bitcion Price Tracker/Controller/ViewController.swift
+++ Bitcion Price Tracker/Controller/ViewController.swift
@@ -16,9 +16,14 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
     
     @IBOutlet weak var priceLabelOutlet: UILabel!
     
+    var currencySymbol:String = ""
+    
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     
+    let bitCoinDataModel = BitCoinDataModel()
+    
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+        let currencySymbolArray = ["A$", "R$","C$","¥","€","£","HK$","Rp","₪","₹","¥","Mex$","kr","$","zł","lei","₽","kr","S$","$","R"]
     var finalURL = ""
     
     
@@ -29,6 +34,12 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         // Very important peiece of code where we are registering our class as delegate and data source for this currency picker.
         currencyPicker.dataSource = self
         currencyPicker.delegate = self
+        
+        // on load UI should show data related to first item of the picker, below changes are done for the same.
+        finalURL = baseURL + currencyArray[0]
+        currencySymbol = currencySymbolArray[0]
+        priceLabelOutlet.text = "Loading..."
+        getBitCoinData(url: finalURL)
     }
     
     
@@ -54,19 +65,69 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
     {
         finalURL = baseURL + currencyArray[row]
+        currencySymbol = currencySymbolArray[row]
         print(finalURL)
+        priceLabelOutlet.text = "Loading..."
+        getBitCoinData(url: finalURL)
     }
     
     
     //MARK: - Networking
     /***************************************************************/
-    
+    func getBitCoinData(url:String)
+    {
+        Alamofire.request(url, method: .get, parameters: nil).responseJSON { response in
+            
+            if(response.result.isSuccess)
+            {
+                print("We are Succcessfully able to call the bitcoin service.")
+                
+                let bitCoinJSON :JSON = JSON(response.result.value!)
+                
+                print(bitCoinJSON)
+                self.updateBitCoinData(jsonValue: bitCoinJSON)
+                
+            }
+            
+            if (response.result.isFailure)
+            {
+                print("Its a failure to call the bitcoin URL \(String(describing: response.result.error))")
+                self.priceLabelOutlet.text = "Connection Error"
+            }
+        }
+        
+    }
     
 
     
     //MARK: - JSON Parsing
     /***************************************************************/
+    
+    func updateBitCoinData(jsonValue:JSON)
+    {
+        // below Optional wrapping is required to avoid app crashing incase we get error code from the api call
+        if let bitCoinPrice = jsonValue["last"].double
+        {
+            bitCoinDataModel.bitCoinPrice = bitCoinPrice
+            // below method will update all UI labels and image views
+            updateUIWithBitCoinData()
+        }
+        else
+        {
+            priceLabelOutlet.text = "Price Value Unavailable"
+            
+        }
+        
+    }
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    
+    func updateUIWithBitCoinData()
+    {
+        priceLabelOutlet.text = "\(currencySymbol)\(String(bitCoinDataModel.bitCoinPrice))"
 
+    }
 
 }
 
---
+++ Bitcion Price Tracker/Model/BitCoinDataModel.swift
@@ -0,0 +1,19 @@
+//
+//  BitCoinDataModel.swift
+//  Bitcion Price Tracker
+//
+//  Created by Vikas R S on 8/9/18.
+//  Copyright © 2018 Vikas Radhakrishna Shetty. All rights reserved.
+//
+
+import Foundation
+
+class BitCoinDataModel
+{
+    var bitCoinPrice:Double
+    
+    init()
+    {
+        bitCoinPrice = 0.0
+    }
+}
